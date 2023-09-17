0d537ca583b846bd91058a75563bf317c6026141
tanvirsingh1 <tnvir2182002@gmail.com>
2023-09-17 15:13:27 -0400

    api added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-
+import { getUserFragments } from './api';
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
   const loginBtn = document.querySelector('#login');
   const logoutBtn = document.querySelector('#logout');
+
+
+  // Do an authenticated request to the fragments API server and log the result
+
 
   // Wire up event handlers to deal with login and logout.
   loginBtn.onclick = () => {
@@ -22,6 +26,7 @@ async function init() {
 
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
+  getUserFragments(user);
   if (!user) {
     // Disable the Logout button
     logoutBtn.disabled = true;
