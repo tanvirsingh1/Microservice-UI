5ec67f691eb6f26358fe77f7022245ab9efc323e
tanvirsingh1 <tnvir2182002@gmail.com>
2023-12-07 23:32:46 -0500

    UI-Complete

--- src/api.js
+++ src/api.js
@@ -59,6 +59,62 @@ export async function PostUserFragment( userInfo, mimeType, fragmentData) {
   }
 }
 
+
+export async function deleteFragment(userInfo, ID) {
+  const requestOptions = {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${userInfo.idToken}`,
+    },
+  };
+
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${ID}`, requestOptions);
+    if(res.status==200)
+    {
+      return res.json()
+    }
+      try {
+        const error = await res.json();
+        console.log(`Error is ${error.message}`);
+        return error;
+      } catch (jsonError) {
+        throw new Error(`HTTP Error: ${res.status} ${res.statusText}`);
+      }
+
+   
+  } catch (error) {
+    console.error("Error deleting fragment:", error);
+    throw error;
+  }
+}
+export async function putFragment( userInfo, ID, type , data) {
+  console.log("Updating user fragments data...");
+  try{
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${userInfo.idToken}`,
+        "Content-Type": type,
+      },
+      body: data,
+    };
+
+    const response = await fetch(`${apiUrl}/v1/fragments/${ID}`, requestOptions);
+    if (!response.ok) {
+      throw new Error(`${response.status} ${response.statusText}`);
+
+    }
+  
+    return await response.text();
+
+  } catch(err){
+    console.error("Failed to Update fragment", err);
+    throw err
+  }
+  
+}
+
 export async function getFragment(user, id) {
   console.log("Requesting user fragments data...");
   try {
@@ -83,7 +139,13 @@ export async function getFragment(user, id) {
         } else if (headers.includes("text/html")) {
           return await res.text(); // HTML can be handled as text as well
         } else if (headers.includes("application/json")) {
-          return await res.json(); // For JSON, use the .json() method
+          return await res.json(); 
+        }
+        else if (headers.includes("image/")){
+          const data = await res.blob();
+          console.log("Got user fragments data",  res );
+        
+      return data
         }
       }
     }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
-import { getUserFragments, PostUserFragment, getFragment, getFragmentMetadata} from './api';
+import { getUserFragments, PostUserFragment, getFragment, getFragmentMetadata, deleteFragment, putFragment} from './api';
 async function init() {
   // Get our UI elements
   const userSection = document.querySelector('#user');
@@ -11,6 +11,9 @@ async function init() {
   const postButton = document.querySelector("#post");
   const getById = document.querySelector("#getbyid");
   const getByInfo = document.querySelector("#getbyid-info")
+  const deleteBtn = document.querySelector("#delete")
+  const updatebtn = document.querySelector('#update')
+
   // Do an authenticated request to the fragments API server and log the result
   
 
@@ -91,13 +94,25 @@ postButton.onclick = async (event) => {
       console.error('An error occurred while posting the fragment:', error);
       alert('Failed to create the fragment.');
     }
+  }else if (contentType.startsWith('image/')) {
+    const photoInput = document.getElementById('photoInput');
+    const file = photoInput.files[0];
+
+    if (file) {
+      try {
+        const fragId = await PostUserFragment(user, contentType, file);
+        alert(`Created a fragment with ID: ${fragId} at ${new Date().toLocaleTimeString()}`);
+      } catch (error) {
+        console.error('An error occurred while posting the image:', error);
+        alert('Failed to create the image fragment.');
+      }
+    } else {
+      alert('Please select an image to upload.');
+    }
   } else {
-    console.error('Unsupported content type. Only "text/plain", "text/markdown", "text/html", and "application/json" are allowed.');
-    alert(`Unsupported content type. Please select a valid content type.`);
+    alert('Unsupported content type.');
   }
 };
-
-
 getById.onclick = async () => {
   const value = document.getElementById('fragment-id').value;
   const res = await getFragment(user, value);
@@ -108,7 +123,16 @@ getById.onclick = async () => {
   if ( res === 'Unsupported Media type, Only (text/plain) is supported[use .txt]' || res == 'Fragment not Found') {
       // Highlighting the error messages
       fragmentsList.innerHTML = `<span style="color:red;">${res}</span>`;
-  } else {
+  }
+  else if (res instanceof Blob) {
+    console.log("Url is ",URL.createObjectURL(res) )
+    var htmlSegment = `<div class="convertedFragment">
+      <img src="${URL.createObjectURL(res)}" alt="Image Fragment"></img>
+    </div>`;
+    const imageElement = document.getElementById('image');
+    fragmentsList.innerHTML = htmlSegment; 
+  }
+  else {
       fragmentsList.innerHTML = "Data of Fragment is: ";
       fragmentsList.textContent += JSON.stringify(res);
   }
@@ -129,6 +153,74 @@ else{
 }
 
 }
+deleteBtn.onclick = async () => {
+  const value = document.getElementById('delete-fragment-id').value;
+  const result = await deleteFragment(user, value);
+  const fragmentsList = document.getElementById('delete-fragment-id');
+ 
+  console.log("result is ",result)
+    // Successful deletion
+    if(result.status === "ok"){
+    document.getElementById('delete-fragment-id').value ="Sucessfully Deleted Fragment";
+    document.getElementById('delete-fragment-id').style.color = 'green';
+    }
+    else{
+    // Error during deletion
+ 
+    document.getElementById('delete-fragment-id').value = "Invalid ID, not Found!"
+    document.getElementById('delete-fragment-id').style.color = 'red';
+    }
+  
+};
+
+updatebtn.onclick = async () => {
+  const ID = document.getElementById('update-fragment-id').value;
+  var contentType = document.getElementById('update-fragment-content-type').value;
+  var content = document.getElementById("update-fragment-content").value
+  if (["text/plain", "text/markdown", "text/html", "application/json"].includes(contentType)) {
+    try{
+   const result = await putFragment(
+      user,ID, contentType, content
+    );
+    console.log("result is ",result)
+    var Presult = JSON.parse(result)
+   
+      document.getElementById('update-fragment-id').value = "Fragment Updated";
+      document.getElementById('update-fragment-id').style.color = 'green';
+      }
+      catch(err) {
+      // Error during deletion
+   
+      document.getElementById('update-fragment-id').value = err.message
+      document.getElementById('update-fragment-id').style.color = 'red';
+      }
+
+  }
+  
+  else if (contentType.startsWith('image/')) {
+    const photoInput = document.getElementById('photoInput2');
+    const file = photoInput.files[0];
+
+    if (file) {
+      try {
+        const fragId = await putFragment(user,ID, contentType, file);
+      
+      } catch (error) {
+        console.error('An error occurred while updating the image:', error);
+        alert('Failed to update the image fragment.');
+      }
+    } else {
+      alert('Please select an image to upload.');
+    }
+  }
+  else {
+    console.error('Unsupported content type. Only "text/plain", "text/markdown", "text/html", and "application/json" are allowed.');
+    alert(`Unsupported content type. Please select a valid content type.`);
+  }
+  
+};
+
+
 }
 
 // Wait for the DOM to be ready, then start the app
